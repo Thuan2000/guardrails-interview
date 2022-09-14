4b053730416f75eb095710e75b22fd5059bcc41b
Thuan Nguyen <nhthuan20@gmail.com>
2022-09-14 20:26:18 +0700

    added Jest configgurations for dashboard

---
+++ dashboard/__tests__/index.test.tsx
@@ -0,0 +1,13 @@
+/**
+ * Copyright Thuan Nguyen ©2022
+ * Good codes make the world a better place!
+ */
+
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+
+describe("Testing The Test Library", () => {
+  it("renders a heading", () => {
+    expect(true).toEqual(true);
+  });
+});
